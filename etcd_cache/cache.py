188591b82d567b4c4e4192075fe659f11c393889
Vietor Davis <vietor.davis@cielo24.com>
2015-06-17 13:29:16 -0700

    PAss host and port correctly to client init

--- etcd_cache/cache.py
+++ etcd_cache/cache.py
@@ -44,7 +44,7 @@ class EtcdCache(BaseCache):
         if hasattr(self, '_client'):
             return self._client
 
-        return Client(self.host)
+        return Client(host=self.host[0], port=self.host[1])
 
     def delete(self, key, version=None):
         path = self._get_path(key, version=None)
